bf59465a2ae51f741c0531bbf6a4a2b60aaa75be
leandrocgsi <leandrocgsi@gmail.com>
2023-08-29 09:25:58 -0300

    First Steps in JUnit 5 with Kotlin

--- S03_FirstStepsInJUnit5WithKotlin/automated-tests-with-kotlin/src/test/kotlin/FooBarTest.kt
+++ S03_FirstStepsInJUnit5WithKotlin/automated-tests-with-kotlin/src/test/kotlin/FooBarTest.kt
@@ -1,2 +1,11 @@
-class FooBarTest {
+package br.com.erudio
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+internal class FooBarTest {
+    @Test
+    fun test() {
+        Assertions.fail<Any>("Not yet implemented")
+    }
 }
