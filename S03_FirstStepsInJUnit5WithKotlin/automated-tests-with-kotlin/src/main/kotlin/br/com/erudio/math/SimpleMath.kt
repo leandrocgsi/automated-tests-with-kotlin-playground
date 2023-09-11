1f4c59146b6fc3ffb66f4ac3ddfef8c5ac22ae24
leandrocgsi <leandrocgsi@gmail.com>
2023-09-11 10:01:17 -0300

    First Steps in JUnit 5 with Kotlin

--- S03_FirstStepsInJUnit5WithKotlin/automated-tests-with-kotlin/src/main/kotlin/br/com/erudio/math/SimpleMath.kt
+++ S03_FirstStepsInJUnit5WithKotlin/automated-tests-with-kotlin/src/main/kotlin/br/com/erudio/math/SimpleMath.kt
@@ -1,4 +1,30 @@
 package br.com.erudio.math
 
+import kotlin.math.sqrt
+
 class SimpleMath {
+    fun sum(firstNumber: Double, secondNumber: Double): Double {
+        return firstNumber + secondNumber
+    }
+
+    fun subtraction(firstNumber: Double, secondNumber: Double): Double {
+        return firstNumber - secondNumber
+    }
+
+    fun multiplication(firstNumber: Double, secondNumber: Double): Double {
+        return firstNumber * secondNumber
+    }
+
+    fun division(firstNumber: Double, secondNumber: Double): Double {
+        if (secondNumber == 0.0) throw ArithmeticException("Impossible to divide by zero!")
+        return firstNumber / secondNumber
+    }
+
+    fun mean(firstNumber: Double, secondNumber: Double): Double {
+        return (firstNumber + secondNumber) / 2
+    }
+
+    fun squareRoot(number: Double?): Double {
+        return sqrt(number!!)
+    }
 }
