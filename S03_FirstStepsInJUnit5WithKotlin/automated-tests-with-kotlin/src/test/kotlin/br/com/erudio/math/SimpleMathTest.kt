dc1ecc94e7a4a611183b5cbc0a0812ef2143a88e
leandrocgsi <leandrocgsi@gmail.com>
2023-09-12 10:02:20 -0300

    First Steps in JUnit 5 with Kotlin

---
+++ S03_FirstStepsInJUnit5WithKotlin/automated-tests-with-kotlin/src/test/kotlin/br/com/erudio/math/SimpleMathTest.kt
@@ -0,0 +1,16 @@
+package br.com.erudio.math
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+
+internal class SimpleMathTest {
+    
+    @Test
+    fun testDivision() {
+        val math = SimpleMath()
+        val result = math.division(6.2, 2.0)
+        Assertions.assertEquals(3.1, result)
+    }
+}
+
