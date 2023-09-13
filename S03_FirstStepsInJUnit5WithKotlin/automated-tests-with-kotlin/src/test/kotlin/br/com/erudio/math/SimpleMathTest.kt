00c5ba31bea1b4d9813190cd358ea38ec0bd2f3c
leandrocgsi <leandrocgsi@gmail.com>
2023-09-13 10:04:09 -0300

    First Steps in JUnit 5 with Kotlin

--- S03_FirstStepsInJUnit5WithKotlin/automated-tests-with-kotlin/src/test/kotlin/br/com/erudio/math/SimpleMathTest.kt
+++ S03_FirstStepsInJUnit5WithKotlin/automated-tests-with-kotlin/src/test/kotlin/br/com/erudio/math/SimpleMathTest.kt
@@ -3,14 +3,13 @@ package br.com.erudio.math
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 
-
 internal class SimpleMathTest {
-    
+
     @Test
     fun testDivision() {
         val math = SimpleMath()
         val result = math.division(6.2, 2.0)
-        Assertions.assertEquals(3.1, result)
+        Assertions.assertEquals(3.1, result, "The testDivision() did not produce expected result!")
     }
 }
 
